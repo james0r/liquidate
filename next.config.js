0fdbaa37116f7f59d1d819eefa121a6e2a4c0d1e
James <james.auble@gmail.com>
2023-11-11 14:07:27 +0900

    brought in more components. properly rendering product, search and complete home page

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.shopify.com"],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'cdn.shopify.com',
         port: '',
-        pathname: '/files/**/*',
+        pathname: '/**/*',
       },
     ],
   },
