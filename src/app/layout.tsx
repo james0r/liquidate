c71181d38139600ff598751ac42d84d0443025ac
James <james.auble@gmail.com>
2023-11-12 16:51:08 +0900

    added geistsans font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import Navbar from "@/components/layout/navbar"
 import { ensureStartsWith } from '@/lib/utils'
-import { Inter } from "next/font/google"
+import { GeistSans } from 'geist/font'
 import { Suspense } from "react"
 import "./globals.css"
 
 import type { ReactNode } from "react"
-
-const inter = Inter({ subsets: ["latin"] })
 
 const { TWITTER_CREATOR, TWITTER_SITE, SITE_NAME } = process.env;
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
@@ -41,7 +39,7 @@ export const metadata = {
 
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" className="">
+    <html lang="en" className={GeistSans.variable}>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
         <Navbar />
         <Suspense>
